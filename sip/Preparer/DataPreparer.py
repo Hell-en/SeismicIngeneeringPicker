d40ee72712ee1320223ec318f642f11dcc07c1d2
Obersht Elena <74058771+Hell-en@users.noreply.github.com>
2024-09-26 16:53:15 +0700

    Update DataPreparer.py

--- sip/Preparer/DataPreparer.py
+++ sip/Preparer/DataPreparer.py
@@ -1,1 +1,50 @@
+class Preparer:
 
+@staticmethod
+    def _gen_set(path, df_header_subset):
+        x = segyrw.read_sgy_traces(path, df_header_subset['IDX'].astype(int).values)
+        y = df_header_subset['FB_NTC'].astype(int).values
+    
+        x = dsp.normalize_traces_by_std(x, 255, axis=1)
+        x = dsp.normalize_traces(x, scale_type='std')
+        x = np.expand_dims(x, axis=-1)
+    
+        heaviside = to_categorical(y+1, num_classes=x.shape[1])
+        y_map = np.cumsum(heaviside, axis=1)
+        heaviside = to_categorical(y-1, num_classes=x.shape[1])
+        y_zeros = np.fliplr(np.cumsum(np.fliplr(heaviside), axis=1))
+        y_pick = to_categorical(y, num_classes=x.shape[1])
+
+        y_mask = np.stack((y_zeros, y_pick, y_map), axis=2)
+
+        y_det = to_categorical(y_pick, num_classes=2)
+
+        heaviside = to_categorical(y, num_classes=x.shape[1])
+        y_map = np.cumsum(heaviside, axis=1)
+        y_heavi = to_categorical(y_map, num_classes=2)
+        return x, y_pick, y_det, y_mask, y_heavi
+    
+
+    def _create_df(self):
+        df_header = pd.read_csv(self.path)
+
+        n = round(len(df_header.index)*0.2)
+        df_header_subset = df_header.sample(n, random_state=37)
+        df_header_subset['FB_NTC'] = np.int32(df_header_subset['FB']*500)
+
+        all_inds = df_header.index.values
+        train_inds = df_header_subset.index.values
+        test_inds = np.setdiff1d(all_inds, train_inds)
+
+        df_header_test = df_header.iloc[test_inds]
+        df_header_test = df_header.iloc[test_inds].sample(random_state=37) # changed. check if ok
+        df_header_test['FB_NTC'] = df_header_test['FB']*500
+
+        return df_header_subset, df_header_test
+
+
+    def generate_data(self):
+        df_header_subset, df_header_test = Reader.create_df()
+        x, y_pick, y_det, y_mask, y_heavi = Reader._gen_set(self.path, df_header_subset)
+        x_test, y_pick_test, y_det_test, y_mask_test, y_heavi_test = Reader._gen_set(self.path, df_header_test)
+        return x, y_pick, y_mask, x_testtes
